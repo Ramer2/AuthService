3ceacf74caac678d9ad01899eb38ea98a2efcd23
Andrii Sysoiev <sisoev2006a@gmail.com>
2025-10-05 12:18:32 +0200

    Given some database info examples

---
+++ sql_example/inserts_example.sql
@@ -0,0 +1,22 @@
+﻿-- those are just some examplery values for the database for easier testing and development
+
+INSERT INTO Role (RoleName)
+VALUES
+    ('Admin'),
+    ('Moderator'),
+    ('User'),
+    ('Guest'),
+    ('Support'),
+    ('Developer');
+
+INSERT INTO Permission (PermissionName)
+VALUES
+    ('ViewUsers'),
+    ('EditUsers'),
+    ('DeleteUsers'),
+    ('ViewReports'),
+    ('GenerateReports'),
+    ('ManageSettings'),
+    ('AccessDashboard'),
+    ('ResetPasswords'),
+    ('AssignRoles');
